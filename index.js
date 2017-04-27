40bb38bdfbbb8263430adc293101fe5d681c32d5
Ivan Aleksandrov <ivan-aleksandrov@users.noreply.github.com>
2017-04-27 23:19:26 +0300

    showing method + application

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 var readline = require('readline');
 var arrayOne = [];
+var arrayApps = [];
+var combinedArr = [];
 // file = name of the log file to be analyzed
-var file = 'server.log.2017-04-26';
+var file = 'server.log.2017-04-25';
 
 var rl = readline.createInterface({
 	input: fs.createReadStream(file),
@@ -12,43 +14,55 @@ var rl = readline.createInterface({
 
 rl.on('line', function (line) {
 	try {
-		var temp = line.split('method":')[1].split(',')[0].trim();
 		// temp = used method in the JSON request
-		arrayOne.push(temp);
+		var temp = line.split('method":')[1].split(',')[0].trim();
 		// fill arrayOne with all methods
+		arrayOne.push(temp);		
+		try {
+			var tempApps = line.split('application":')[1].split(/,|}/)[0].trim();
+		}
+		catch (err) {
+			// tempApps = used application in the JSON request
+			var tempApps = '"NO_APPLICATION"'
+		}
+		arrayApps.push(tempApps);
+		// fill arrayApps with all methods
 	}
 	catch (err) { }
 });
 
 rl.on('close', function () {
-	// sort arrayOne 'a,a,b,c,c,c,d,d,e...etc' and count repetitions.
-	arrayOne.sort();
-	var current = null;
-	var cnt = 0;
-	console.log(file);
-	for (var i = 0; i < arrayOne.length; i++) {
-		if (arrayOne[i] != current) {
-			if (cnt > 0) {
-				console.log(current + ' comes --> ' + cnt + ' times');
-			}
-			current = arrayOne[i];
-			cnt = 1;
-		} else {
-			cnt++;
+	for (var i = 0; i < arrayApps.length; i++) {
+		combinedArr[i] = [arrayOne[i], arrayApps[i], 1];
+	}
+	combinedArr.sort(sortFunction);
+
+	function sortFunction(a, b) {
+		if (a[0] === b[0]) {
+			return 0;
+		}
+		else {
+			return (a[0] < b[0]) ? -1 : 1;
 		}
 	}
-	if (cnt > 0) {
-		console.log(current + ' comes --> ' + cnt + ' times');
+	//Evaluate each element of the combinedArr with preceding. It stops if it finds match. The matched preceding element gets ++ on its counter, and the evaluated element gets its counter=0
+	for (var i = 1; i < combinedArr.length; i++) {
+		for (var j = 0; j < i; j++) {
+			if (combinedArr[j][0] == combinedArr[i][0] && combinedArr[j][1] == combinedArr[i][1]) {
+				combinedArr[j][2]++;
+				combinedArr[i][2] = 0;
+				break;
+			}
+		}
 	}
-	console.log('\r\nTotal count of requests --> ' + arrayOne.length);
-});
+	var tempCount = 0;
+	console.log(file);
+	for (var i = 0; i < combinedArr.length; i++) {
+		if (combinedArr[i][2] != 0) {
+			console.log('Method: ' + combinedArr[i][0] + ' Application: ' + combinedArr[i][1] + ' --> ' + combinedArr[i][2] + ' times.');
+			tempCount+=combinedArr[i][2];
+		}
 
-// example output:
-// server.log.2017-04-25
-// "DICT" comes --> 14 times
-// "GET_RECORD" comes --> 538 times
-// "OFS" comes --> 1070 times
-// "SAVE_RECORD" comes --> 3819 times
-// "SELECT" comes --> 2341 times
-// "SIGN_ON" comes --> 1 times
-// "VALIDATE_RECORD" comes --> 9793 times
+	}
+	console.log('Total requests: '+tempCount);
+});
